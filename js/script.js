ac9bc5c7ff13b3c97ce8c0a7dedc4f6cfa3bb4ac
Mohd Rejoan <wearecrx@gmail.com>
2024-11-24 23:02:41 +0600

    reverse timer script created

--- js/script.js
+++ js/script.js
@@ -11,22 +11,60 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   };
 
-  const LargeSlider = new Swiper("#customer-review-slider", configration);
-  const SmallSlider = new Swiper("#review-slider-sm", configration);
+  new Swiper("#customer-review-slider", configration);
+  new Swiper("#review-slider-sm", configration);
 
   // accordion
   const accordionElement = document.querySelector(".accordion");
-  if (accordionElement) {
-    const accordionHead = accordionElement.querySelector(".accordion-head");
-    const accordionBody = accordionElement.querySelector(".accordion-body");
-    const accordionBtn = accordionHead.querySelector("button");
+  const accordionHead = accordionElement.querySelector(".accordion-head");
+  const accordionBody = accordionElement.querySelector(".accordion-body");
+  var bodyHeight = accordionBody.offsetHeight;
 
-    if (window.innerWidth >= 1280) {
-      accordionElement.classList.add("show");
-    } else {
-      accordionBtn.addEventListener("click", function () {
-        accordionElement.classList.toggle("show");
-      });
+
+  function RegisterAccordion() {
+    if (accordionElement) {
+      const accordionBtn = accordionHead.querySelector("button");
+
+      if (window.innerWidth >= 1280) {
+        accordionElement.classList.add("show");
+      } else {
+        accordionBtn.addEventListener("click", function () {
+          accordionElement.classList.toggle("show");
+        });
+      }
     }
   }
+
+  RegisterAccordion();
+
+  window.addEventListener("resize", RegisterAccordion);
+
+  // reverse timer 
+  let totalSeconds = 14 * 60 + 58; 
+
+  function updateTimer() {
+    const bookingTimer = document.querySelector("#booking-timer");
+    const hoursElement = bookingTimer.querySelector('.hours h2');
+    const minutesElement = bookingTimer.querySelector('.munis h2');
+    const secondsElement = bookingTimer.querySelector('.secs h2');
+
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    hoursElement.textContent = hours.toString().padStart(2, '0');
+    minutesElement.textContent = minutes.toString().padStart(2, '0');
+    secondsElement.textContent = seconds.toString().padStart(2, '0');
+
+    if (totalSeconds <= 0) {
+      clearInterval(timerInterval);
+    }
+
+    totalSeconds--;
+  }
+
+  const timerInterval = setInterval(updateTimer, 1000);
+  updateTimer();
+
+
 });
