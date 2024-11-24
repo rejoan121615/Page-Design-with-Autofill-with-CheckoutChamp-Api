6b1220fcf55dee5c2848c455ec3035b42b77f2fa
Mohd Rejoan <wearecrx@gmail.com>
2024-11-24 13:27:18 +0600

    swiper js slider implemented

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var splide = new Splide('.splide');
-    splide.mount();
-  } );
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper("#customer-review-slider", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    autoHeight: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".next-btn",
+      prevEl: ".prev-btn",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+});
